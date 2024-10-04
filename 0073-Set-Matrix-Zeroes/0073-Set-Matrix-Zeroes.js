09ee449e503b93e99a16a8d9da1c1edc270b316b
lokappakishan <lokappakishan@gmail.com>
2024-10-04 21:56:48 +0200

    added solution to 0073 leetcode problem

--- 0073-Set-Matrix-Zeroes/0073-Set-Matrix-Zeroes.js
+++ 0073-Set-Matrix-Zeroes/0073-Set-Matrix-Zeroes.js
@@ -34,6 +34,51 @@ n == matrix[0].length
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
+
 var setZeroes = function(matrix) {
-    
+    let isCol = false;
+    let rows = matrix.length;
+    let cols = matrix[0].length;
+
+    // Traverse the matrix
+    for (let i = 0; i < rows; i++) {
+        // Check if the first column needs to be zeroed
+        if (matrix[i][0] === 0) {
+            isCol = true;
+        }
+        
+        // Mark rows and columns to be zeroed
+        for (let j = 1; j < cols; j++) {
+            if (matrix[i][j] === 0) {
+                matrix[i][0] = 0; // Mark the row
+                matrix[0][j] = 0; // Mark the column
+            }
+        }
+    }
+
+    // Zero out marked rows and columns
+    for (let i = 1; i < rows; i++) {
+        for (let j = 1; j < cols; j++) {
+            if (matrix[i][0] === 0 || matrix[0][j] === 0) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+
+    // Zero out the first row if needed
+    if (matrix[0][0] === 0) {
+        for (let j = 0; j < cols; j++) {
+            matrix[0][j] = 0;
+        }
+    }
+
+    // Zero out the first column if needed
+    if (isCol) {
+        for (let i = 0; i < rows; i++) {
+            matrix[i][0] = 0;
+        }
+    }
+
 };
+
+console.log(setZeroes([[0,1,2,0],[3,4,5,2],[1,3,1,5]]));
